1c0becdd36038b8842b4252fadcaeee3a696d55f
Dario <dariojahic95@gmail.com>
2016-06-11 14:22:05 +0200

    Zavrsena metoda koja predvija naredna tri karaktera unetog stringa.

--- CollectionsHomework/src/main/Bigram.java
+++ CollectionsHomework/src/main/Bigram.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 
 public class Bigram {
 
-	public void ispisiFrekPojavljivanjaBigrama(String s) {
+	public Map<String, Integer> ispisiFrekPojavljivanjaBigrama(String s) {
 		Map<String, Integer> m = new HashMap<>();
 		for (int i = 0; i < s.length()-2; i++) {
 			String s2 = s.substring(i, i+2);
@@ -27,6 +27,8 @@ public class Bigram {
 			}
 		}
 		ispisiElementeMape(m);
+		//koristimo return da bi mogli da iskoristimo datu mapu za sledeci zadatak predvidjanja 3 karaktera.
+		return m;
 	}
 	public void ispisiElementeMape(Map<String, Integer> m ){
 		Iterator entries = m.entrySet().iterator();
@@ -37,11 +39,56 @@ public class Bigram {
 		  System.out.println(key + ": " + value);
 		}
 	}
-
+	public void predvidiNarednaTriKaraktera(String s, Map<String, Integer> m){
+		int a = 0;
+		String s2 = s.substring(s.length()-1);
+		while(a <3 ){
+		Iterator entries = m.entrySet().iterator();
+		//Map<String, Integer> m2  = new HashMap<>();
+		int [] frekvencije = new int[100];
+		int i = 0;
+		//Iterator entries2 = m2.entrySet().iterator();
+		while (entries.hasNext()) {
+		  Entry thisEntry = (Entry) entries.next();
+		  String key = (String) thisEntry.getKey();
+		  int value = (int) thisEntry.getValue();
+		  if(key.startsWith(s2)){
+			  frekvencije[i] = value;
+			  i++;
+		  }
+		}
+		//i je brEl
+		int max = nadjiNajveci(frekvencije, i);
+		entries = m.entrySet().iterator();
+		while (entries.hasNext()) {
+			  Entry thisEntry = (Entry) entries.next();
+			  String key = (String) thisEntry.getKey();
+			  int value = (int) thisEntry.getValue();
+			  if(value == max && key.startsWith(s2)){
+				  s2 = key.substring(key.length()-1);
+				  System.out.println(s2);
+				  a++;
+				  break;
+			  }
+			}
+		
+		}
+	}
+	private int nadjiNajveci(int[] frekvencije, int i) {
+		int max = frekvencije[0];
+		for (int j = 0; j< frekvencije[j]; j++) {
+			if(frekvencije[j]>max){
+				max = frekvencije[j];
+			}
+		}
+		return max;
+	}
 	public static void main(String[] args) {
 		Bigram b = new Bigram();
-		b.ispisiFrekPojavljivanjaBigrama("abbcceeeeeeabcc");
-
+		Map<String, Integer> m = b.ispisiFrekPojavljivanjaBigrama("abbcceeeeeeabcc");
+		b.predvidiNarednaTriKaraktera("ja", m);
+		
+	
 	}
 
 }
